ab91f53114344e746d24684b2c28ae550943036c
rageqqq <chestersoh5@hotmail.com>
2021-03-11 21:57:50 +0800

    v1.0 Chester
    Added basic commands to food recommendation component (add,delete,find,print)
    Created new food store class

---
+++ src/main/java/seedu/duke/foodstore/FoodStore.java
@@ -0,0 +1,34 @@
+package seedu.duke.foodstore;
+
+public class FoodStore {
+
+    public final String storeName;
+    public final String storeDescription;
+    public boolean isHalal;
+
+    public FoodStore(String storeName, String storeDescription, boolean isHalal) {
+        this.storeName = storeName.trim();
+        this.storeDescription = storeDescription.trim();
+        this.isHalal = isHalal;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public String getStoreDescription() {
+        return storeDescription;
+    }
+
+    public boolean isHalal() {
+        return isHalal;
+    }
+
+    public String toString() {
+        if (isHalal()) {
+            return "[H]" + getStoreName() + ": " + getStoreDescription();
+        } else {
+            return "[X]" + getStoreName() + ": " + getStoreDescription();
+        }
+    }
+}
