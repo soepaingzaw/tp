a1405a859848e54a396b23c3e2f50a0ab446680f
rageqqq <chestersoh5@hotmail.com>
2021-03-13 23:25:17 +0800

    v1.01
    Fixed Java CI requirements
    Added printWelcomeMessage and printAvailableCommands methods

--- src/main/java/seedu/duke/FoodRecommendation.java
+++ src/main/java/seedu/duke/FoodRecommendation.java
@@ -7,11 +7,18 @@ import java.util.List;
 
 public class FoodRecommendation {
 
-    private static final String UNABLE_TO_FIND_MESSAGE = "Sorry, We are unable to find any food stores that fit the description";
+    private static final String FIND_ERROR_MESSAGE =
+            "Sorry, We are unable to find any food stores that fit the description";
+    private static final String WELCOME_MESSAGE = "Welcome to Food Recommendation Bot!\n"
+            + "Enter help to find out all available commands";
+    private static final String AVAILABLE_COMMANDS = "Here are the list of commands: \n"
+            + "1. add [store name] [store description] - Adds a new food store\n"
+            + "2. list - Prints out all the food stores\n"
+            + "3. delete [store index] - Deletes the store with that index";
 
     private final List<FoodStore> foodStoreList = new ArrayList<>();
 
-    public FoodRecommendation(){
+    public FoodRecommendation() {
 
     }
 
@@ -41,7 +48,20 @@ public class FoodRecommendation {
         if (foodStoreList.contains(toFind)) {
             printStore(toFind);
         } else {
-            System.out.println(UNABLE_TO_FIND_MESSAGE);
+            System.out.println(FIND_ERROR_MESSAGE);
         }
+    }
+
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+
+    public void printAvailableCommands() {
+        System.out.println(AVAILABLE_COMMANDS);
+    }
+
+    public void run() {
+        printWelcomeMessage();
+        printAvailableCommands();
     }
 }
